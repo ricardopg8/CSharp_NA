959f98f18ba5c813800b508482ba63b94a98ce24
ricardopg8 <ricardopg8@gmail.com>
2024-06-17 19:11:16 -0300

    Classes 23 to 30

---
+++ Course/Conditional-structure [If-Else]/ConditionalS2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Globalization;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Qual a hora?");
+
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12) {
+                Console.WriteLine("Bom dia");
+            } 
+            else if ((hora >= 12) && (hora < 18)) {
+                Console.WriteLine("Boa tarde");
+            }
+            else { Console.WriteLine("Boa noite");
+            }
+        }
+    }
+}
