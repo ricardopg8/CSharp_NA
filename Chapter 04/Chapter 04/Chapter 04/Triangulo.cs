13bf4ec377b6cf82b6bf4fa0e42982b690da5ee3
ricardopg8 <ricardopg8@gmail.com>
2024-07-01 20:52:36 -0300

    Agora vai

---
+++ Chapter 04/Chapter 04/Chapter 04/Triangulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_04 {
+    internal class Triangulo {
+
+        public double A;
+        public double B;
+        public double C;
+    }
+}
