fce9ffeec83b76d195301bd2ff073a2b117936ae
ricardopg8 <ricardopg8@gmail.com>
2024-09-08 16:48:40 -0300

    Turning on in the Desktop 01

---
+++ 42 Começando a resolver um segundo problema exemplo/CAM_Ex02/CAM_Ex02/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Globalization;
+
+namespace CAM_Ex02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Funcionario func = new Funcionario();
+
+            Console.Write("Nome.: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário bruto.: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto.: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine("Funcionario: " + func);
+
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salario em qual porcentagem? ");
+            double percent = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(percent);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+           
+        }
+    }
+}
