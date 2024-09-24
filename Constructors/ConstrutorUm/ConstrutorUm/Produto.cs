6fb8a318ab356cceff034aa90efb3d1c058093c7
ricardopg8 <50276326+ricardopg8@users.noreply.github.com>
2024-09-24 15:52:37 -0300

    Revert "Construtor incremento this note"

--- Constructors/ConstrutorUm/ConstrutorUm/Produto.cs
+++ Constructors/ConstrutorUm/ConstrutorUm/Produto.cs
@@ -11,18 +11,10 @@ namespace ConstrutorUm {
         public double Preco;
         public int Quantidade;
 
-        public Produto(string nome, double preco, int quantidade) {
+        public Produto (string nome, double preco, int quantidade) {
             Nome = nome;
             Preco = preco;
             Quantidade = quantidade;
-        }
-
-        //public Produto(string nome, double preco) {
-        //    Nome = nome;
-        //    Preco = preco;
-        //}
-
-        public Produto() {
         }
 
         public double ValorTotalEmEstoque() {
